491ce9ae427a20998fa30193a20fddbb4f2572ce
Nathan REBOURG <nathanreb69@gmail.com>
2025-01-18 20:04:31 +0100

    extraction
    rectification de certains disfonctionnements

--- Downloads/SAE 1.05/12/extraction seance.py
+++ Downloads/SAE 1.05/12/extraction seance.py
@@ -1,7 +1,12 @@
-def extraire_r107_groupe(tableau_csv, groupe):
+def extraire_r107(tableau_csv, groupe):
     resultats = []
     for ligne in tableau_csv:
         champs = ligne.split(";")
         if "R1.07" in champs[5] and groupe in champs[8]:
             resultats.append(f"{champs[1]};{champs[3]};{champs[4]}")
     return resultats
+
+tableau_csv = convertir_calendrier_ics_en_csv("calendrier.ics")
+seances_r107 = extraire_r107(tableau_csv, "A1")
+for ligne in seances_r107:
+    print(ligne)
